d8df52b8e1d3c36b65b61cad610c00d93661610c
J-RAP <SrRapero720@hotmail.com>
2024-04-10 00:51:46 -0600

    fix REI is not working

--- src/main/java/me/srrapero720/embeddiumplus/mixins/impl/jei_rei_emi/ReiOverlayMixin.java
+++ src/main/java/me/srrapero720/embeddiumplus/mixins/impl/jei_rei_emi/ReiOverlayMixin.java
@@ -21,21 +21,14 @@ import java.util.List;
 @Mixin(value = ScreenOverlayImpl.class, priority = 500, remap = false)
 @Pseudo
 public class ReiOverlayMixin {
-    @Unique private EntryListWidget embPlus$entryListWidget;
-
-    @WrapOperation(method = "init", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"))
-    public boolean redirect$addOnList(List<Widget> instance, Object e, Operation<Boolean> original) {
-        if (e instanceof EntryListWidget listWidget) {
-            embPlus$entryListWidget = listWidget;
-            return false;
+    @WrapOperation(method = "renderWidgets", at = @At(value = "INVOKE", target = "Lme/shedaniel/rei/api/client/gui/widgets/Widget;render(Lnet/minecraft/client/gui/GuiGraphics;IIF)V"))
+    private void redirect$rendering(Widget instance, GuiGraphics graphics, int mouseX, int mouseY, float deltaTick, Operation<Void> original) {
+        if (instance instanceof EntryListWidget) {
+            if (!EmbyConfig.hideJREMICache || !REIRuntimeImpl.getSearchField().getText().isEmpty()) {
+                original.call(instance, graphics, mouseX, mouseY, deltaTick);
+            }
         } else {
-            return original.call(instance, e);
+            original.call(instance, graphics, mouseX, mouseY, deltaTick);
         }
-    }
-
-    @Inject(method = "renderWidgets", at = @At("TAIL"))
-    public void inject$bottom(GuiGraphics graphics, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        if (EmbyConfig.hideJREMICache && REIRuntimeImpl.getSearchField().getText().isEmpty()) return;
-        embPlus$entryListWidget.render(graphics, mouseX, mouseY, delta);
     }
 }
