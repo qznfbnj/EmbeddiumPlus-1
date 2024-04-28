8a484678a255c6ad2010dd1b12b2f2a4bb460eff
J-RAP <SrRapero720@hotmail.com>
2024-04-28 08:34:20 -0600

    copypasta

--- src/main/java/me/srrapero720/embeddiumplus/util/MockerInstalledException.java
+++ src/main/java/me/srrapero720/embeddiumplus/util/MockerInstalledException.java
@@ -2,6 +2,6 @@ package me.srrapero720.embeddiumplus.util;
 
 public class MockerInstalledException extends RuntimeException {
     public MockerInstalledException() {
-        super("§6§lEmbeddium Extras §ris incompatible and not supported, please remove it");
+        super("§6§lEmbeddium Extras §ris incompatible, please remove it");
     }
 }
